309955b497bb193141d9eb421e2869a65881c716
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-04-07 14:33:18 +0200

    added dependencies

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.example.weatherapp
 
+import android.app.VoiceInteractor
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okio.IOException
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val apiKey = "6e88eafae4cebe1a2a7de5aedb56ee7b"
+    private lateinit var weatherService: WeatherService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        weatherService = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherService::class.java)
+
+        // Replace "CityName" with the desired city
+        GlobalScope.launch(Dispatchers.IO) {
+            val weatherData = weatherService.getWeather("Warsaw", apiKey)
+            withContext(Dispatchers.Main) {
+                updateUI(weatherData)
+            }
         }
     }
+
+    private fun updateUI(weatherData: WeatherData) {
+        findViewById<TextView>(R.id.textViewCity).text = weatherData.name
+        findViewById<TextView>(R.id.textViewTemperature).text =
+            "${weatherData.main.temp.toInt()}°C"
+        val iconUrl = "https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png"
+        Glide.with(this)
+            .load(iconUrl)
+            .into(findViewById(R.id.imageViewWeatherIcon))
+    }
 }
+
+data class WeatherData(
+    val name: String,
+    val main: Main,
+    val weather: List<Weather>
+)
+
+data class Main(
+    val temp: Double
+)
+
+data class Weather(
+    val icon: String
+)
